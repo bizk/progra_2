34e5ebcf823c85789cafd0d6dbbd331534f4b912
Fedejp <37599222+Fedejp@users.noreply.github.com>
2018-06-14 23:55:04 -0300

    todo lindo todo listo

---
+++ src/tps/tp3/TestPunto52.java
@@ -0,0 +1,46 @@
+package tps.tp3;
+import api.ConjuntoTDA;
+import api.DiccionarioMultipleTDA;
+import api.DiccionarioSimpleTDA;
+import implementaciones.listas.*;
+import utilidades.Metodos;
+
+public class TestPunto52 {
+	public static void main (String[] args) {
+		DiccionarioSimpleTDA DS = new DiccionarioSimpleLD();
+		
+		Metodos m = new Metodos();
+		
+		DS.Agregar(4994839, 10);
+		DS.Agregar(7897554, 4);
+		DS.Agregar(4768558, 5);
+		DS.Agregar(4561225, 10);
+		DS.Agregar(1245215, 5);
+		DS.Agregar(4587445, 4);
+		DS.Agregar(478552, 7);
+		
+		mostrar(m.DiccionarioSimpleAMultiple(DS));
+	}
+	public static void mostrar(DiccionarioMultipleTDA original) {
+		int x, y;
+		ConjuntoTDA claves = new ConjuntoLD();
+		claves.InicializarConjunto();
+		ConjuntoTDA valores = new ConjuntoLD();
+		valores.InicializarConjunto();
+		
+		claves = original.Claves();
+		while(!claves.ConjuntoVacio()) {
+			x = claves.ElegirConjunto();
+			System.out.print("\n C " + x);
+			valores = original.Recuperar(x);
+			System.out.print(": ");
+			while(!valores.ConjuntoVacio()) {
+				y = valores.ElegirConjunto();
+				System.out.print(y + " ");
+				valores.SacarConjunto(y);
+			}
+			claves.SacarConjunto(x);
+		}
+		System.out.println("\n");
+	}
+}
